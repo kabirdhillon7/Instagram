eac45fe1432dfc05126c7cd5c933f164f802d019
kabirdhillon7 <kabirdhillon99@gmail.com>
2021-02-27 22:07:42 -0800

    Initial commit

---
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -0,0 +1,23 @@
+package com.example.instagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+
+public class ParseApplication extends Application {
+
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("zt6C8Ze5ILqeVuKuwez0vimzPiO7aOXPsmUysDOG")
+                .clientKey("buMwnUXluqVgeNxjlgJfyxOBKiQt3t2LU7LmvBZM")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
